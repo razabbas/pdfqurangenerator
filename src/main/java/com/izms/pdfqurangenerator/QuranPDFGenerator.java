1eb90106e76d439f538a5a00b8c24b27fbd7fe96
Raza Abbas <rkabbas@gmail.com>
2021-07-29 23:54:13 -0400

    adding command line arguments. Next logging, error handling, documentation

--- src/main/java/com/izms/pdfqurangenerator/QuranPDFGenerator.java
+++ src/main/java/com/izms/pdfqurangenerator/QuranPDFGenerator.java
@@ -8,7 +8,12 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
-import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType0Font;
 
@@ -19,26 +24,27 @@ import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
 
 public class QuranPDFGenerator {
+
+	private static String outputFileName;
+	private static String quranMetadataFileXml;
+	private static String quranTextFile;
 	
-	private static String outputFileName = "HolyQuran.pdf";
-	private static String quranMetadataFileXml = "quranmetadata/quran-data.xml";
-	private static String arabicFontFile = "font/ar/NotoNaskhArabic-hinted/NotoNaskhArabicUI-Bold.ttf";
+	private static String arabicFontFile;
+	private static float arabicFontSize;
+
+	private static String englishFontFile = "font/en/NotoSans-hinted/NotoSans-Medium.ttf";
+	private static float englishFontSize = 10;
 	
-	private static float arabicFontSize = 18;
-	private static String englishFontFile="font/en/NotoSans-hinted/NotoSans-Medium.ttf";
-	private static float englishFontSize=10;
-	
-	private static String quranTextFile="arabic/quran-simple.txt";
-	
-	private static String translationTextFile="translations/fr.hamidullah.txt";
-	private static String translationFontFile="font/fr/NotoSansDisplay-hinted/NotoSansDisplay-Medium.ttf";
-	private static float translationFontSize=10;
-	private static boolean translationRtl= false;
-	
+	private static String translationTextFile;
+	private static String translationFontFile;
+	private static float translationFontSize;
+	private static boolean translationRtl;
+
 	private static String BISMILLAH = "ِسْمِ اللَّهِ الرَّحْمَـٰنِ الرَّحِيمِ";
 
-	public static void main(String... arg) throws JAXBException, IOException, URISyntaxException {
-		
+	public static void main(String... args) throws JAXBException, IOException, URISyntaxException, ParseException {
+
+		processCommandLine(args);
 		Quran quranMetaData = readQuranMetadataJaxB(quranMetadataFileXml);
 		PDFBook quranPdf = new PDFBook(outputFileName, PageType.LETTER, 20, 20);
 		// add the fontfiles to the
@@ -47,7 +53,6 @@ public class QuranPDFGenerator {
 		PDFont translationFont = addFontFile(quranPdf, translationFontFile);
 		quranPdf.setHeaderFooterFont(translationFont, translationFontSize);
 		quranPdf.setSpaceFont(arabicFont, arabicFontSize);
-		
 
 		BufferedReader arabicQuranReader = getFileBufferedReader(quranTextFile);
 		BufferedReader translationQuranReader = getFileBufferedReader(translationTextFile);
@@ -75,28 +80,28 @@ public class QuranPDFGenerator {
 				if (suraNumber > 1) {
 					quranPdf.addPage();
 				}
-				quranPdf.writeLine(sura.getName(), arabicFont, arabicFontSize, TextAlignment.CENTER,true);
+				quranPdf.writeLine(sura.getName(), arabicFont, arabicFontSize, TextAlignment.CENTER, true);
 				quranPdf.writeLine(sura.getEname(), englishFont, englishFontSize, TextAlignment.CENTER);
 				quranPdf.writeLine(
 						"Number:" + sura.getIndex() + " Type:" + sura.getType() + " Num of Ayat:" + sura.getAyas(),
 						englishFont, englishFontSize, TextAlignment.CENTER);
 				quranPdf.writeBlankLine(1);
 				previousSuraNumber = suraNumber;
-				
-				if (suraNumber> 1 && ayat.contains(BISMILLAH)) {
+
+				if (suraNumber > 1 && ayat.contains(BISMILLAH)) {
 					ayat = ayat.substring(BISMILLAH.length());
-					quranPdf.writeLine(BISMILLAH, arabicFont, arabicFontSize, TextAlignment.CENTER,true);
+					quranPdf.writeLine(BISMILLAH, arabicFont, arabicFontSize, TextAlignment.CENTER, true);
 				}
-				
+
 			}
-			
-			
-			quranPdf.writeLine(ayat+"\uFD3F"+ayatNumber+"\uFD3E", arabicFont, arabicFontSize, TextAlignment.CENTER,true);
-			quranPdf.writeLine(translation, translationFont, translationFontSize, TextAlignment.CENTER,translationRtl);
+
+			quranPdf.writeLine(ayat + "\uFD3F" + ayatNumber + "\uFD3E", arabicFont, arabicFontSize,
+					TextAlignment.CENTER, true);
+			quranPdf.writeLine(translation, translationFont, translationFontSize, TextAlignment.CENTER, translationRtl);
 			quranPdf.writeBlankLine(1);
 			arabicLineParts = arabicQuranReader.readLine().trim().split("\\u007C");
 			translationLineParts = translationQuranReader.readLine().trim().split("\\u007C");
-		
+
 		}
 
 		quranPdf.closeDocument();
@@ -126,6 +131,84 @@ public class QuranPDFGenerator {
 		URL fontResource = POCApp.class.getClassLoader().getResource(fontFileName);
 		File fontFile = new File(fontResource.toURI());
 		return book.addFontFile(fontFile);
+	}
+
+	private static void processCommandLine(String[] args) {
+		// create the command line parser
+		CommandLineParser parser = new DefaultParser();
+		// create the Options
+		Options options = new Options();
+		options.addOption("o", "output-file-name", true, "output pdf file");
+		options.addOption("q", "quran-metadata-xmlfile", true, "quran metadata xml file");
+		options.addOption("a", "arabic-font-file", true, "arabic font file");
+		options.addOption("s", "arabic-font-size", true, "arabic font size");
+		options.addOption("t", "quran-text-file", true, "quran text file");
+		options.addOption("r", "translation-font-file", true, "translation font file");
+		options.addOption("z", "translation-font-size", true, "translation font size");
+		options.addOption("x", "translation-text-file", true, "translation text file");
+		options.addOption("l", "translation-rtl", true, "is translation right to left");
+		try {
+			CommandLine cmd = parser.parse(options, args);
+			if (cmd.hasOption("o")) {
+				outputFileName = cmd.getOptionValue("o");
+			} else {
+				outputFileName = "HolyQuran.pdf";
+			}
+			
+			if (cmd.hasOption("q")) {
+				quranMetadataFileXml = cmd.getOptionValue("q");
+			} else {
+				quranMetadataFileXml = "quranmetadata/quran-data.xml";
+			}
+			
+			if (cmd.hasOption("a")) {
+				arabicFontFile = cmd.getOptionValue("a");
+			} else {
+				arabicFontFile = "font/ar/NotoNaskhArabic-hinted/NotoNaskhArabicUI-Bold.ttf";
+			}
+			
+			if (cmd.hasOption("s")) {
+				arabicFontSize = Float.parseFloat(cmd.getOptionValue("s"));
+			} else {
+				arabicFontSize = 18f;
+			}
+			
+			if (cmd.hasOption("t")) {
+				quranTextFile = cmd.getOptionValue("t");	
+			} else {
+				quranTextFile = "arabic/quran-simple.txt";
+			}
+			
+			if (cmd.hasOption("r")) {
+				translationFontFile = cmd.getOptionValue("r");
+			} else {
+				translationFontFile = "font/en/NotoSans-hinted/NotoSans-Medium.ttf";
+			}
+			
+			if (cmd.hasOption("z")) {
+				translationFontSize = Float.parseFloat(cmd.getOptionValue("z"));	
+			} else {
+				translationFontSize = 10f;
+			}
+			
+			if (cmd.hasOption("x")) {
+				translationTextFile = cmd.getOptionValue("x");
+			} else {
+				translationTextFile = "translations/en.ahmedali.txt";
+			}
+			
+			if (cmd.hasOption("l")) {
+				translationRtl = Boolean.parseBoolean(cmd.getOptionValue("l"));
+			} else {
+				translationRtl = false;
+			}
+			
+		} catch (ParseException e) {
+			System.out.println("Error in command line arguments");
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("java QuranPDFGenerator", options);
+			System.exit(1);
+		}
 	}
 
 }
